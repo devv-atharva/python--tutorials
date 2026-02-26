bfd949e34cce7ee5a08db489a64d1499efa6c140
Atharva Takawale <takawaleatharva047@gmail.com>
2026-02-26 22:05:28 +0530

    Day 2

---
+++ Day 2.py
@@ -0,0 +1,22 @@
+name = "Atharva"
+age = 21
+height = 5.11
+old = False
+a = None
+learning = ["Python" , "Github"]
+
+
+print("Name:" ,name)
+print("Age:", age)
+print("Height:" , height)
+print("Is old :" , old)
+print(a)
+print("Learning:" , learning)
+
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(old))
+print(type(a))
+print(type(learning))
